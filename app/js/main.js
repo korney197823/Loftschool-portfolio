ffa8da01395b0891bf1a24638fc48a02f7aa99ab
Kornienko Denis <korney197823@gmail.com>
2016-05-25 23:04:02 +0300

    make slicky footer and html-tamplate mobile version form.html

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
   var burger = $('.menu__toggle');
   var menu = $('.menu');
 
-  burger.click(function(){
-    menu.toggle('menu--close');
-  })
+  burger.click(
+    function() {
+      menu.toggle();
+    }
+  );
 });
